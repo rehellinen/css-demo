61f67d3a883276f4a36fbbdcd7a322e77fcb5c15
rehellinen <rehellinen@foxmail.com>
2019-10-18 10:12:15 +0800

    float demo

--- src/components/float/float.vue
+++ src/components/float/float.vue
@@ -1,9 +1,9 @@
 <template lang="pug">
   div
-    .note
+    //.note
       p float的初衷只是为了实现文字环绕效果
-    .note
-      p 包裹性（其实就是BFC）
+    //.note
+      p 包裹性
       p 若内部有float:left元素，外部容器宽度为100%，高度会塌陷
       p 但是当外部容器使用了float:left的时候
       p 就会产生收缩（水平）、坚挺（垂直）、隔绝的效果，这种效果称为包裹性
@@ -11,40 +11,29 @@
       p (1) display: inline-block / table-cell
       p (2) position: absolute / fixed / sticky
       p (3) overflow: hidden / scroll
-    .note
+    //.note
       p 破坏性
       p 其他有破坏性的属性：
       p (1) display: none
       p (2) position: absolute / fixed / sticky
-    .note
+    //.note
       p block化
       p 去空格化
-    .note
-      p 图1 - 正常情况
-    .note
-      p 图2 - 破坏性
-      p img使用了float，而容器没有使用
-      p 容器宽度为100%，高度为0
-    .note
-      p 图3 - 包裹性
-      p img和容器都使用了float
-      p 容器宽度和高度都跟图片大小一样
+    p.note 这里实现了文字环绕效果
     div.text-around
       img(src="../../assets/timg.jpg")
-      p 文字环绕效果
-      p 文字环绕效果
-      p 文字环绕效果
-      p 文字环绕效果
-      p 文字环绕效果
-      p 文字环绕效果
-      p 文字环绕效果
-      p 文字环绕效果
-      p 文字环绕效果
-
-    //.img-float
+      p 文字环绕效果，文字环绕效果，文字环绕效果，文字环绕效果
+    p.note 图片设置了浮动，父元素高度塌陷
+    .wrapper-bfc
+      .img-float
+        img(src="../../assets/timg.jpg")
+    p.note 使用生成BFC的方法清除浮动
+    .wrapper-bfc
       img(src="../../assets/timg.jpg")
-    //.wrapper-float
+    p.note 使用clear属性清除浮动
+    .wrapper-clear
       img(src="../../assets/timg.jpg")
+      .clear
 </template>
 
 <script>
@@ -54,24 +43,27 @@ export default {
 
 <style lang="sass" scoped>
   .text-around
-    &::after
-      content: ''
-      display: block
-      clear: both
-      height: 0
-      overflow: hidden
+    width: 130px
     img
       float: left
       width: 70px
+    p
+      background-color: lightblue
   .img-float
-    float: left
+    background-color: lightblue
     img
       float: left
       width: 200px
-  .wrapper-float
-    position: absolute
+  .wrapper-bfc
+    overflow: hidden
     img
       float: left
       width: 200px
-  .de
+  .wrapper-clear
+    img
+      float: left
+      width: 200px
+    .clear
+      clear: both
+    .de
 </style>
